28af8b0971fb5108a435f4893f1c5f63ebe8cd3c
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-04-23 22:40:06 -0300

    feat: add consulting methods

--- src/app/App.java
+++ src/app/App.java
@@ -1,12 +1,30 @@
 package app;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+import db.DBException;
 
 public class App {
 	public static void main(String[] args) {
-		Connection connection = DB.getConnection();
+		Connection connection = null;
+		Statement st = null;
+		ResultSet rs = null;
+
+		try {
+			connection = DB.getConnection();
+			st = connection.createStatement();
+			rs = st.executeQuery("select * from department");
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id")+" - "+rs.getString("Name"));
+			}
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		}
+
 		DB.closeConnection();
 	}
 }
