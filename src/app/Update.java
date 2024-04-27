78aadb78752dbdce631f060c1937e1d0842a59ac
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-04-26 23:20:16 -0300

    feat: update implementaded

--- src/app/Update.java
+++ src/app/Update.java
@@ -1,10 +1,45 @@
 package app;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import db.DB;
+import db.DBException;
+
 public class Update {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Connection connection = null;
+		PreparedStatement ps = null;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite o nome do funcionário que receberá um aumento: ");
+		String employerName = sc.nextLine();
+		System.out.print("Valor do aumento: ");
+		double raise = sc.nextDouble();
+		try {
+			connection = DB.getConnection();
+			ps = connection.prepareStatement(
+					"UPDATE seller "
+					+"SET BaseSalary = BaseSalary + ? "
+					+"WHERE Name= ? "
+					);
+			ps.setDouble(1, raise);
+			ps.setString(2, employerName);
+			
+			ps.executeUpdate();
+			
+			System.out.println("Aumento concedido com sucesso!!");
+			
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		}finally {
+			sc.close();
+			DB.closeStatement(ps);
+			DB.closeConnection();
+		}
 	}
 
 }
